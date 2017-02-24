a66bf2f0eeb55a3778c15a2cb7a6b88aba409757
Victor Stinner <vstinner@redhat.com>
2017-02-24 20:02:43 +0100

    Fix bench_fastcall_deque_methods on Python 3.4

--- bench_fastcall_deque_methods.py
+++ bench_fastcall_deque_methods.py
@@ -7,6 +7,7 @@ http://bugs.python.org/issue29452
 Created at 2017-02-05 by Victor STINNER.
 """
 
+import collections
 import perf
 
 runner = perf.Runner()
@@ -19,11 +20,15 @@ runner.timeit('collections.deque.rotate(1)',
               'd.rotate(1)',
               setup='import collections; d=collections.deque()',
               duplicate=100)
-runner.timeit('collections.deque.insert()',
-              'd = collections.deque(); d.insert(0, None); d.insert(1, None); d.insert(2, None); d.insert(3, None); d.insert(4, None)',
-              setup='import collections',
-              duplicate=10)
-runner.timeit('collections.deque([None]).index(None)',
-              'd.index(None)',
-              setup='import collections; d = collections.deque([None])',
-              duplicate=100)
+if hasattr(collections.deque, 'index'):
+    # Python 3.5+
+    runner.timeit('collections.deque([None]).index(None)',
+                  'd.index(None)',
+                  setup='import collections; d = collections.deque([None])',
+                  duplicate=100)
+if hasattr(collections.deque, 'insert'):
+    # Python 3.5+
+    runner.timeit('collections.deque.insert()',
+                  'd = collections.deque(); d.insert(0, None); d.insert(1, None); d.insert(2, None); d.insert(3, None); d.insert(4, None)',
+                  setup='import collections',
+                  duplicate=10)
