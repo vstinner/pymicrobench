a952cf47749dc051ba502d4b2a5183d6c5199c0d
Victor Stinner <vstinner@redhat.com>
2017-02-24 18:44:32 +0100

    Add bench_fastcall_c_method

---
+++ bench_fastcall_c_method.py
@@ -0,0 +1,23 @@
+"""
+Benchmark on calling C methods.
+
+http://bugs.python.org/issue29263
+
+Created at 2017-01-14 by INADA Naoki.
+"""
+
+import perf
+
+runner = perf.Runner()
+runner.timeit('b"".decode()',
+              "empty_bytes.decode()",
+              setup="empty_bytes = b''",
+              duplicate=100)
+runner.timeit('b"".decode("ascii")',
+              "empty_bytes.decode('ascii')",
+              setup="empty_bytes = b''",
+              duplicate=100)
+runner.timeit("[0].count(0)",
+              "my_list.count(0)",
+              setup="my_list = [0]",
+              duplicate=100)
