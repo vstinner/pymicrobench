ce85e974cb3eb6bc9e143042b43d0f9562d1d7bd
Victor Stinner <vstinner@redhat.com>
2017-02-24 19:19:54 +0100

    Add bench_fastcall_struct.py

---
+++ bench_fastcall_struct.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Benchmark on the struct module for FASTCALL.
+
+http://bugs.python.org/issue29300
+
+Created at 2017-02-02 by Victor STINNER.
+"""
+
+import perf
+
+runner = perf.Runner()
+
+runner.timeit('int.to_bytes(1, 4, "little")',
+              'to_bytes(1, 4, "little")',
+              setup='to_bytes = int.to_bytes',
+              duplicate=100)
+
+runner.timeit('struct.pack("i", 1)',
+              'pack("i", 1)',
+              setup='import struct; pack = struct.pack',
+              duplicate=100)
