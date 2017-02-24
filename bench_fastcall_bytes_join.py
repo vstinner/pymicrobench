df22d00c7bd1c0c9129a7fd8b4416650cd5e1c79
Victor Stinner <vstinner@redhat.com>
2017-02-24 19:46:55 +0100

    Add bench_fastcall_bytes_join

---
+++ bench_fastcall_bytes_join.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Benchmark on the struct module for FASTCALL.
+
+http://bugs.python.org/issue28004
+
+Created at 2016-09-07 by Victor STINNER.
+"""
+
+import perf
+
+runner = perf.Runner()
+
+
+runner.timeit('b"".join((b"hello", b"world"))',
+              'sep.join(seq)',
+              setup="sep=b''; seq=(b'hello', b'world')",
+              duplicate=100)
+runner.timeit('b"".join((b"hello", b"world") * 100)',
+              'sep.join(seq)',
+              setup="sep=b''; seq=(b'hello', b'world', b'. ') * 100",
+              duplicate=100)
