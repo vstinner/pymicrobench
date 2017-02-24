76ca781ad50485ad2dd8152d002dc101e83f27ea
Victor Stinner <vstinner@redhat.com>
2017-02-24 18:46:20 +0100

    test that all bench names are unique

--- run_tests.py
+++ run_tests.py
@@ -1,10 +1,28 @@
+import contextlib
 import glob
+import os
 import subprocess
 import sys
+import tempfile
 
 
-def test(script):
-    args = [sys.executable, script, "-p1", "-v"]
+@contextlib.contextmanager
+def temporary_file(**kwargs):
+    tmp_filename = tempfile.mktemp(**kwargs)
+    try:
+        yield tmp_filename
+    finally:
+        try:
+            os.unlink(tmp_filename)
+        except OSError as exc:
+            if exc.errno != errno.ENOENT:
+                raise
+
+
+def test(script, output):
+    # write all results into a file to ensure that all test
+    # names are unique
+    args = [sys.executable, script, "-p1", "-v", "--append", output]
     print("+ " + ' '.join(args))
     proc = subprocess.run(args)
     if proc.returncode:
@@ -12,8 +30,9 @@ def test(script):
 
 
 def run_tests():
-    for script in glob.glob("bench*.py"):
-        test(script)
+    with temporary_file() as tmpfile:
+        for script in glob.glob("bench*.py"):
+            test(script, tmpfile)
 
 
 if __name__ == "__main__":
