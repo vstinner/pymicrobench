7a7711d7903f56ba6de1c5b0d51c8a35e0efdadc
Victor Stinner <victor.stinner@gmail.com>
2017-04-19 02:16:40 +0200

    Add bench_logging_silent
    Copied from https://github.com/python/performance

---
+++ bench_logging_silent.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+Measure the overhead of a logging call doing nothing.
+"""
+
+import io
+import logging
+
+import six
+from six.moves import xrange
+import perf
+
+FORMAT = 'important: %s'
+MESSAGE = 'some important information to be logged'
+
+
+def bench_logging_silent(loops, logger, stream):
+    # micro-optimization: use fast local variables
+    m = MESSAGE
+    range_it = xrange(loops)
+    t0 = perf.perf_counter()
+
+    for _ in range_it:
+        # repeat 10 times
+        logger.debug(m)
+        logger.debug(m)
+        logger.debug(m)
+        logger.debug(m)
+        logger.debug(m)
+        logger.debug(m)
+        logger.debug(m)
+        logger.debug(m)
+        logger.debug(m)
+        logger.debug(m)
+
+    dt = perf.perf_counter() - t0
+
+    if stream.getvalue():
+        raise ValueError("stream is expected to be empty")
+
+    return dt
+
+
+if __name__ == "__main__":
+    runner = perf.Runner()
+    runner.metadata['description'] = ("Measure the overhead of "
+                                      "a logging call doing nothing")
+
+    if six.PY3:
+        stream = io.StringIO()
+    else:
+        stream = io.BytesIO()
+
+    handler = logging.StreamHandler(stream=stream)
+    logger = logging.getLogger("benchlogger")
+    logger.propagate = False
+    logger.addHandler(handler)
+    logger.setLevel(logging.WARNING)
+
+    runner.bench_time_func('logging_silent', bench_logging_silent,
+                           logger, stream, inner_loops=10)
